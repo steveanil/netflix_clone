39325a567c7397af6e974675f2708160390c98d6
Steve <steveanil2003@gmail.com>
2022-12-07 00:22:03 -0800

    implemented sidebar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { AppBar, IconButton, Toolbar, Drawer, Button, Avatar, useMediaQuery } from '@mui/material';
 import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material';
 import { Link } from 'react-router-dom'; // allows us to switch between different pages
 import { useTheme } from '@mui/material/styles';
 
+import { Sidebar } from '..';
 import useStyles from './styles';
 
 const NavBar = () => {
+  const [MobileOpen, setMobileOpen] = useState(false);
   // gives access to all of the styles in styles.js
   const classes = useStyles();
   const isMobile = useMediaQuery('(max-width:600px)'); // if something is larger that 600px it is not mobile
@@ -14,48 +16,72 @@ const NavBar = () => {
   const isAuthenticated = true;
 
   return (
-    <AppBar position="fixed">
-      <Toolbar className={classes.toolbar}>
-        {isMobile && (
+    <>
+      <AppBar position="fixed">
+        <Toolbar className={classes.toolbar}>
+          {isMobile && (
           <IconButton
             color="inherit"
             edge="start"
             style={{ outline: 'none' }}
-            onClick={() => {}}
+            onClick={() => setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
             className={classes.menuButton}
           >
             <Menu />
           </IconButton>
-        )}
-        <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
-          {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
-        </IconButton>
-        {!isMobile && 'Search...'}
-        <div>
-          {!isAuthenticated ? (
-            <Button color="inherit" onClick={() => {}}>
-              Login &nbsp; <AccountCircle />
-            </Button>
-          ) : (
-            <Button
-              color="inherit"
-              component={Link}
-              to="/profile/123"
-              className={classes.linkButton}
-              onClick={() => {}}
-            >
-              {!isMobile && <>My Movies &nbsp;</>}
-              <Avatar
-                style={{ width: 30, height: 30 }}
-                alt="Profile"
-                src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Favatar-icon-placeholder-facebook-1577909%2F&psig=AOvVaw1JX2xOKi8d_a4_lu3wwBi_&ust=1669539156175000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCICQgZW8y_sCFQAAAAAdAAAAABAD"
-              />
-            </Button>
           )}
-        </div>
-        {isMobile && 'Search...'}
-      </Toolbar>
-    </AppBar>
+          <IconButton color="inherit" sx={{ ml: 1 }} onClick={() => {}}>
+            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+          </IconButton>
+          {!isMobile && 'Search...'}
+          <div>
+            {!isAuthenticated ? (
+              <Button color="inherit" onClick={() => {}}>
+                Login &nbsp; <AccountCircle />
+              </Button>
+            ) : (
+              <Button
+                color="inherit"
+                component={Link}
+                to="/profile/123"
+                className={classes.linkButton}
+                onClick={() => {}}
+              >
+                {!isMobile && <>My Movies &nbsp;</>}
+                <Avatar
+                  style={{ width: 30, height: 30 }}
+                  alt="Profile"
+                  src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fvectors%2Favatar-icon-placeholder-facebook-1577909%2F&psig=AOvVaw1JX2xOKi8d_a4_lu3wwBi_&ust=1669539156175000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCICQgZW8y_sCFQAAAAAdAAAAABAD"
+                />
+              </Button>
+            )}
+          </div>
+          {isMobile && 'Search...'}
+        </Toolbar>
+      </AppBar>
+      <div>
+        <nav className={classes.drawer}>
+          {isMobile ? (
+          // if On mobile
+            <Drawer
+              variant="temporary"
+              anchor="right"
+              open={MobileOpen}
+              onClose={() => setMobileOpen((prevMobileOpen) => !prevMobileOpen)}
+              classes={{ paper: classes.drawerPaper }}
+              ModalProps={{ keyMounted: true }}
+            >
+              <Sidebar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          ) : (
+          // if not mobile
+            <Drawer classes={{ paper: classes.drawerPaper }} variant="permanent" open>
+              <Sidebar setMobileOpen={setMobileOpen} />
+            </Drawer>
+          )}
+        </nav>
+      </div>
+    </>
   );
 };
 
